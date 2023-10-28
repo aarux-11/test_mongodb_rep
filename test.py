e9204351792e1e9f8f7ffaccbb423daacb984d6f
Aarushi Kumar <71340243+aarux-11@users.noreply.github.com>
2023-10-28 14:25:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,14 +9,13 @@ db = client.youtube_data
 
 st.write("Here's our first attempt at using data to create a table:")
 data = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
+    'first_column': [1, 2, 3, 4],
+    'second_column': [10, 20, 30, 40]
 })
 
 st.table(data)
-data = pd.DataFrame(data)
 
 if st.button("Upload to MongoDB"):
     with st.spinner('Please Wait for it...'):
         collections1 = db.data_details
-        collections1.insert_many(data)
+        collections1.insert_many(pd.DataFrame(data, columns=list('first_column','second_column')))
