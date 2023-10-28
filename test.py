8c5bd459ff1720ebd5f6753083a83badaaed4e2c
Aarushi Kumar <71340243+aarux-11@users.noreply.github.com>
2023-10-28 14:29:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 #Bridging a connection with MongoDB Atlas and Creating a new database(youtube_data)
 client = pymongo.MongoClient("mongodb+srv://Aarushi:'%40%40rushi1108'@youtubeproject.nrit3zy.mongodb.net/?retryWrites=true&w=majority")
-db = client.youtube_data
+db = client[youtube_data]
 
 st.write("Here's our first attempt at using data to create a table:")
 data = pd.DataFrame({
@@ -18,4 +18,4 @@ st.table(data)
 if st.button("Upload to MongoDB"):
     with st.spinner('Please Wait for it...'):
         collections1 = db.data_details
-        collections1.insert_many(pd.DataFrame(data, columns=list('first_column')))
+        collections1.insert_many(pd.DataFrame(pd.read_csv(data)
