df90b232e5ae67e05c4f000711bf1523ae1f6c8f
Aarushi Kumar <71340243+aarux-11@users.noreply.github.com>
2023-10-28 16:46:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -23,14 +23,7 @@ collection_name = dbname["channel_details"]
 
 st.write("Here's our first attempt at using data to create a table:")
 
-data = {
-"Channel_Name": "Example Channel",
-"Channel_Id": "UC1234567890",
-"Subscription_Count": 10000,
-"Channel_Views": 1000000,
-"Channel_Description": "This is an example channel.",
-"Playlist_Id": "PL1234567890"
-}
+data = [["Channel_Name", "Example Channel"],["Channel_Id", "UC1234567890",]]
 
 data = pd.DataFrame(data, columns=['Channel','Details'])
 
